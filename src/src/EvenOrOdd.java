3e56889fcd56815aa554268c1c95d615ee0213ea
Kang Yeeun <wjsry1emd@@>
2023-02-14 23:41:36 +0900

    Feat : programmers01 알고리즘 문제
    body : 삼항 연산자를 이용하여 짝수와 홀수 문제를 풀었다.

---
+++ src/src/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package src;
+
+public class EvenOrOdd {
+    String evenOrOdd(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(3));
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(2));
+    }
+}
+
+
