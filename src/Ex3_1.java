42cdb346f9985a1dfed07125aaf26f1cf02a8bb4
Kang Yeeun <wjsry1emd@@>
2023-03-01 22:56:18 +0900

    Feat : programmers01 알고리즘 문제 body : Integer.parseInt(String s)를 이용해 문자열을 숫자로 변환

---
+++ src/Ex3_1.java
@@ -0,0 +1,10 @@
+class Ex3_1 {
+    public int solution(String s) {
+      return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Ex3_1 a= new Ex3_1();
+        System.out.println(a.solution("23424"));
+    }
+}
--- src/Ex3_1a.java
+++
@@ -1,7 +0,0 @@
-import static java.lang.Math.abs;
-
-class Ex3_1 {
-    public int solution(String s) {
-      return Integer.parseInt(s);
-    }
-}
